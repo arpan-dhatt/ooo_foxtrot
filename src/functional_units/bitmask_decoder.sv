3f34274a76a2f7d2f678af0a526f2d4fe7b7ca1b
Arpan Dhatt <ary.dhatt@gmail.com>
2024-04-17 22:10:00 -0500

    All code for untested logical functional unit

---
+++ src/functional_units/bitmask_decoder.sv
@@ -0,0 +1,103 @@
+// bitmask decoder for AND immediate
+
+module bitmask_decoder #(
+    parameter int M = 64
+)(
+    input  logic       immN,
+    input  logic [5:0] imms,
+    input  logic [5:0] immr,
+    input  logic       immediate,
+    output logic [M-1:0] wmask,
+    output logic [M-1:0] tmask
+);
+
+    logic [5:0] tmask_and, wmask_and;
+    logic [5:0] tmask_or, wmask_or;
+    logic [5:0] levels;
+    logic [5:0] len;
+    logic [5:0] s, r;
+    logic [6:0] diff;
+
+    always_latch begin
+        // Compute log2 of element size
+        // 2^len must be in range [2, M]
+        len = 6'(1 << $clog2(immN ? ~imms : {6{1'b1}}));
+        if (len < 6'd1) begin
+            wmask = 'x;
+            tmask = 'x;
+        end else begin
+            assert (M >= (1 << len));
+
+            // Determine s, r and s - r parameters
+            levels = (1 << len) - 1;
+
+            // For logical immediates an all-ones value of s is reserved
+            // since it would generate a useless all-ones result (many times)
+            if (immediate && ((imms & levels) == levels)) begin
+                wmask = 'x;
+                tmask = 'x;
+            end else begin
+                s = imms & levels;
+                r = immr & levels;
+                diff = s - r; // 6-bit subtract with borrow
+
+                // Compute "top mask"
+                tmask_and = diff[5:0] | ~levels;
+                tmask_or  = diff[5:0] & levels;
+
+                tmask = {M{1'b1}};
+                tmask = ((tmask 
+                        & {32{tmask_and[0], 1'b1}}) 
+                        | {32{1'b0, tmask_or[0]}});
+                tmask = ((tmask 
+                        & {16{{2{tmask_and[1]}}, {2{1'b1}}}}) 
+                        | {16{{2{1'b0}}, {2{tmask_or[1]}}}});
+                tmask = ((tmask 
+                        & {8{{4{tmask_and[2]}}, {4{1'b1}}}}) 
+                        | {8{{4{1'b0}}, {4{tmask_or[2]}}}});
+                tmask = ((tmask 
+                        & {4{{8{tmask_and[3]}}, {8{1'b1}}}}) 
+                        | {4{{8{1'b0}}, {8{tmask_or[3]}}}});
+                tmask = ((tmask 
+                        & {2{{16{tmask_and[4]}}, {16{1'b1}}}}) 
+                        | {2{{16{1'b0}}, {16{tmask_or[4]}}}});
+                tmask = ((tmask 
+                        & {1{{32{tmask_and[5]}}, {32{1'b1}}}}) 
+                        | {1{{32{1'b0}}, {32{tmask_or[5]}}}});
+
+                // Compute "wraparound mask"
+                wmask_and = immr | ~levels;
+                wmask_or  = immr & levels;
+
+                wmask = {M{1'b0}};
+                wmask = ((wmask 
+                        & {32{{1{1'b1}}, {1{wmask_and[0]}}}}) 
+                        | {32{{1{wmask_or[0]}}, {1{1'b0}}}});
+                wmask = ((wmask 
+                        & {16{{2{1'b1}}, {2{wmask_and[1]}}}}) 
+                        | {16{{2{wmask_or[1]}}, {2{1'b0}}}});
+                wmask = ((wmask 
+                        & {8{{4{1'b1}}, {4{wmask_and[2]}}}}) 
+                        | {8{{4{wmask_or[2]}}, {4{1'b0}}}});
+                wmask = ((wmask 
+                        & {4{{8{1'b1}}, {8{wmask_and[3]}}}}) 
+                        | {4{{8{wmask_or[3]}}, {8{1'b0}}}});
+                wmask = ((wmask 
+                        & {2{{16{1'b1}}, {16{wmask_and[4]}}}}) 
+                        | {2{{16{wmask_or[4]}}, {16{1'b0}}}});
+                wmask = ((wmask 
+                        & {1{{32{1'b1}}, {32{wmask_and[5]}}}}) 
+                        | {1{{32{wmask_or[5]}}, {32{1'b0}}}});
+
+                if (diff[6]) // borrow from s - r
+                    wmask = wmask & tmask;
+                else
+                    wmask = wmask | tmask;
+
+                wmask = wmask[M-1:0];
+                tmask = tmask[M-1:0];
+            end
+        end
+    end
+
+endmodule
--- src/functional_units/fu_logical.sv
+++ src/functional_units/fu_logical.sv
@@ -37,7 +37,7 @@ module fu_logical (
 
   // ANDS, TST
   localparam [10:0] ANDS3121 = 11'b11101010000;
-  localparam [10:0] ANDS1510 = 11'b00000000000;
+  localparam [5:0] ANDS1510 = 6'b000000;
 
   // SBFM, ASR
   localparam [8:0] SBFM3123 = 9'b100100110;
@@ -45,23 +45,103 @@ module fu_logical (
   // UBFM, LSL, LSR
   localparam [8:0] UBFM3123 = 9'b110100110;
 
+  // AND immediate bitmask decoder
+  logic [63:0] dec_wmask, _dec_tmask;
+  bitmask_decoder #(64) andbd (
+    .immN(fu.inst[22]),
+    .imms(fu.inst[15:10]),
+    .immr(fu.inst[21:16]),
+    .immediate(1'b1),
+    .wmask(dec_wmask),
+    .tmask(_dec_tmask)
+  );
+
+  // SBFM UBFM calculation
+  always_comb
+  begin
+    
+  end
+
   always_ff @(posedge fu.clk)
   begin
     if (fu.rst) begin
       // set outputs invalid and ready to 1
       fu.fu_out_valid <= 0;
       fu.fu_ready <= 1;
-    end
-    else if (fu.inst[31:21] == CSEL3121 && fu.inst[11:10] == CSEL1110) begin
-      // CSEL
-      if (fu.inst[15:12] == fu.op[2][3:0]) begin
-        // copy from Xn
-        fu.fu_out_data[0] <= fu.op[0];
-      end else begin
-        // copy from Xm
-        fu.fu_out_data[0] <= fu.op[1];
+    end else if (fu.inst_valid) begin
+      // input instruction is valid
+      if (fu.inst[31:21] == CSEL3121 && fu.inst[11:10] == CSEL1110) begin
+        // CSEL
+        if (fu.inst[15:12] == fu.op[2][3:0]) begin
+          // copy from Xn
+          fu.fu_out_data[0] <= fu.op[0];
+        end else begin
+          // copy from Xm
+          fu.fu_out_data[0] <= fu.op[1];
+        end
+        // Xd is in out_prn 0
+      end else if (fu.inst[31:21] == CSINC3121 && fu.inst[11:10] == CSINC1110) begin
+        // CSINC, CSET, CSINC
+        if (fu.inst[15:12] == fu.op[2][3:0]) begin
+          // copy from Xn
+          fu.fu_out_data[0] <= fu.op[0];
+        end else begin
+          // copy from Xn and add 1
+          fu.fu_out_data[0] <= fu.op[1] + 1;
+        end
+      end else if (fu.inst[31:21] == CSINV3121 && fu.inst[11:10] == CSINV1110) begin
+        // CSINV, CSETM, CINV
+        if (fu.inst[15:12] == fu.op[2][3:0]) begin
+          // copy from Xn
+          fu.fu_out_data[0] <= fu.op[0];
+        end else begin
+          // copy from ~Xn
+          fu.fu_out_data[0] <= ~fu.op[1];
+        end
+      end else if (fu.inst[31:21] == CSNEG3121 && fu.inst[11:10] == CSNEG1110) begin
+        // CSNEG, CNEG
+        if (fu.inst[15:12] == fu.op[2][3:0]) begin
+          // copy from Xn
+          fu.fu_out_data[0] <= fu.op[0];
+        end else begin
+          // copy from -Xn (two's complement)
+          fu.fu_out_data[0] <= ~fu.op[1] + 1;
+        end
+        fu.fu_out_prn[0] <= fu.out_prn[0];
+      end else if (fu.inst[31:21] == MVN3121 && fu.inst[15:05] == MVN1505) begin
+        // MVN
+        fu.fu_out_data[0] <= ~fu.op[0]; // copy ~Xm to Xd
+      end else if (fu.inst[31:21] == ORR3121 && fu.inst[15:10] == ORR1510) begin
+        // ORR
+        fu.fu_out_data[0] <= fu.op[0] | fu.op[1]; // Xd <= Xn | Xm
+      end else if (fu.inst[31:21] == EOR3121 && fu.inst[15:10] == EOR1510) begin
+        // EOR
+        fu.fu_out_data[0] <= fu.op[0] ^ fu.op[1]; // Xd <= Xn ^ Xm
+      end else if (fu.inst[31:22] == AND3122) begin
+        // AND
+        // use decoded wmask
+        fu.fu_out_data[0] <= fu.op[0] & dec_wmask;
+      end else if (fu.inst[31:21] == ANDS3121 && fu.inst[15:10] == ANDS1510) begin
+        // ANDS, TST
+        fu.fu_out_data[0] <= fu.op[0] & fu.op[1]; // Xd <= Xn & Xm
+      end else if (fu.inst[31:23] == SBFM3123) begin
+        // SBFM, ASR
+        if (fu.inst[15:10] >= fu.inst[21:16]) begin
+          fu.fu_out_data[0] <= ({64{fu.op[0][fu.inst[21:16]]}} << (64 - (fu.inst[15:10] - fu.inst[21:16] + 1))) | (fu.op[0] >> fu.inst[21:16]);
+        end else begin
+          fu.fu_out_data[0] <= ({64{fu.op[0][fu.inst[15:10]]}} << (64 - fu.inst[21:16])) | (fu.op[0] << (64 - fu.inst[15:10] - 1));
+        end
+      end else if (fu.inst[31:23] == UBFM3123) begin
+        // UBFM, LSL, LSR
+        if (fu.inst[15:10] >= fu.inst[21:16]) begin
+          fu.fu_out_data[0] <= (fu.op[0] >> fu.inst[21:16]) & ((1 << (fu.inst[15:10] - fu.inst[21:16] + 1)) - 1);
+        end else begin
+          fu.fu_out_data[0] <= (fu.op[0] << (64 - fu.inst[21:16])) & ((1 << (fu.inst[15:10] + 1)) - 1);
+        end
       end
-      // Xd is in out_prn 0
+      
+
+      // everything does the same stuff below here
       fu.fu_out_prn[0] <= fu.out_prn[0];
       fu.fu_out_data_valid <= {1, 0 ,0};
       fu.fu_out_valid <= fu.inst_valid;
