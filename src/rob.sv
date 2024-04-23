0ced156b788bfac913441d3816476b72a97e8cf2
Noah Klayman <noahklayman@gmail.com>
2024-04-23 16:00:44 -0500

    basic rob (no flush support)

--- src/rob.sv
+++ src/rob.sv
@@ -1,29 +1,41 @@
 module rob #(
     parameter INST_ID_BITS = 6,
     parameter PRN_BITS = 6,
-    parameter MAX_OPERANDS = 3
+    parameter MAX_OPERANDS = 3,
+    parameter FU_COUNT = 4
 ) (
     input logic clk,
     input logic rst,
 
+    // Renamer dispatch
     input logic inst_valid,
-    input logic [32:0] inst,
-    input logic [63:0] inst_lrn_input[MAX_OPERANDS],
-    input logic [63:0] inst_lrn_output[MAX_OPERANDS],
+    output logic inst_ready,
+    input logic [63:0] pc,
+    // Note that these mappings are what was ovewritten by this instruction, not what it actually maps to
+    input logic mapping_inputs_valid[MAX_OPERANDS],
+    input logic [PRN_BITS-1:0] mapping_inputs_prn[MAX_OPERANDS],
+    input logic [32:0] mapping_inputs_arn[MAX_OPERANDS],
+    output logic [INST_ID_BITS-1:0]new_inst_id,
 
-    output logic rob_full
+    // Freed prns for the renamer
+    output logic freed_prns_valid[MAX_OPERANDS],
+    output logic [PRN_BITS-1:0] freed_prns[MAX_OPERANDS],
+
+    // From FUs
+    input logic fu_out_inst_valid[FU_COUNT],
+    input logic [INST_ID_BITS-1:0] fu_out_inst_ids[FU_COUNT]
 );
 
-localparam ROB_STATE_ISSUE = 0;
-localparam ROB_STATE_EXECUTE = 1;
-localparam ROB_STATE_WRITE = 2;
-localparam ROB_STATE_COMMIT = 3;
+localparam ROB_STATE_ISSUED = 0;
+localparam ROB_STATE_COMMITED = 1;
+localparam ROB_STATE_RETIRE = 2;
 
 typedef struct {
     logic [1:0] state;
-    logic [32:0] inst;
-    logic [63:0] lrn_input[MAX_OPERANDS];
-    logic [63:0] lrn_output[MAX_OPERANDS];
+    logic [63:0] pc;
+    logic mapping_valid[MAX_OPERANDS];
+    logic [PRN_BITS-1:0] mapping_prn[MAX_OPERANDS];
+    logic [32:0] mapping_arn[MAX_OPERANDS];
 } ROBEntry;
 
 // circular buffer
@@ -37,20 +49,39 @@ begin
         // reset queue
         head <= 0;
         tail <= 0;
-        rob_full <= 0;
-    end else if (inst_valid && !rob_full) begin
-        // insert stuff into buffer
-        buffer[head].state <= ROB_STATE_ISSUE;
-        buffer[head].inst <= inst;
-        buffer[head].lrn_input[0] <= inst_lrn_input[0];
-        buffer[head].lrn_input[1] <= inst_lrn_input[1];
-        buffer[head].lrn_input[2] <= inst_lrn_input[2];
-        buffer[head].lrn_output[0] <= inst_lrn_output[0];
-        buffer[head].lrn_output[1] <= inst_lrn_output[1];
-        buffer[head].lrn_output[2] <= inst_lrn_output[2];
+        inst_ready <= 0;
+    end else begin
+        if (inst_valid && inst_ready) begin
+            // insert stuff into buffer
+            buffer[head].state <= ROB_STATE_ISSUED;
+            buffer[head].pc <= pc;
+            for (int i = 0; i < MAX_OPERANDS; i++) begin
+                buffer[head].mapping_valid[i] <= mapping_inputs_valid[i];
+                buffer[head].mapping_prn[i] <= mapping_inputs_prn[i];
+                buffer[head].mapping_arn[i] <= mapping_inputs_arn[i];
+            end
 
-        head <= head + 1;
-        rob_full <= head + 1 == tail;
+            new_inst_id <= head;
+
+            head <= head + 1;
+            inst_ready <= head + 1 != tail;
+        end
+        if (tail != head && buffer[tail].state == ROB_STATE_COMMITED) begin
+            // commit
+            for (int i = 0; i < MAX_OPERANDS; i++) begin
+                freed_prns_valid[i] <= buffer[tail].mapping_valid[i];
+                freed_prns[i] <= buffer[tail].mapping_prn[i];
+            end
+            $display("Committing instruction at %0d", buffer[tail].pc);
+
+            tail <= tail + 1;
+            inst_ready <= 1;
+        end
+        for (int i = 0; i < FU_COUNT; i++) begin
+            if (fu_out_inst_valid[i]) begin
+                buffer[fu_out_inst_ids[i]].state <= ROB_STATE_COMMITED;
+            end
+        end
     end
 end
 
