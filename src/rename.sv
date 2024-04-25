6a996a854571383016c938bad30434b0a9e07ffa
Arpan Dhatt <ary.dhatt@gmail.com>
2024-04-25 18:53:04 -0500

    Untested renamer (doesn't support back-up on branch mispredict)

--- src/rename.sv
+++ src/rename.sv
@@ -1,122 +1,137 @@
 module rename #(
+    parameter ARN_BITS = 6,
     parameter PRN_BITS = 6,
     parameter MAX_OPERANDS = 3
 ) (
     input logic clk,
     input logic rst,
 
-    input logic lrns_valid,
-    input logic [5:0] lrn_input[MAX_OPERANDS],
-    input logic [5:0] lrn_output[MAX_OPERANDS],
+    input logic input_valid, // inputs to rename are valid
+    // arn's from decoder
+    input logic [ARN_BITS-1:0] arn_input[MAX_OPERANDS],
+    input logic [ARN_BITS-1:0] arn_output[MAX_OPERANDS],
 
-    input logic free_valid[MAX_OPERANDS * 2],
-    input logic [PRN_BITS-1:0] free_prns[MAX_OPERANDS * 2],
+    // PRN's being freed by ROB
+    input logic free_valid[MAX_OPERANDS],
+    input logic [PRN_BITS-1:0] free_prns[MAX_OPERANDS],
 
+    // PRN inputs
+    output logic prn_input_valid[MAX_OPERANDS],
     output logic [PRN_BITS-1:0] prn_input[MAX_OPERANDS],
+
+    // PRN outputs
+    output logic prn_output_valid[MAX_OPERANDS],
     output logic [PRN_BITS-1:0] prn_output[MAX_OPERANDS],
-    output logic prns_valid
+
+    // renamer output is valid (false if not enough PRN's, so stall fe/d)
+    output logic mapping_valid,
+
+    // stuff overwritten by instruction for the ROB (will commit them)
+    output logic mapping_inputs_valid[MAX_OPERANDS],
+    output logic [PRN_BITS-1:0] mapping_inputs_prn[MAX_OPERANDS],
+    output logic [ARN_BITS-1:0] mapping_inputs_arn[MAX_OPERANDS]
 );
 
-localparam OP_BITS = $clog2(MAX_OPERANDS);
-localparam [5:0] INVALID_LRN = 62;
-localparam [5:0] ZERO_LRN = 63;
+localparam MO_BITS = $clog2(MAX_OPERANDS);
 
-// maximum of PRN_BITS**2 PRN's can be available at a time 
-logic [PRN_BITS-1:0] free_prn_fifo[1 << PRN_BITS];
-logic [PRN_BITS-1:0] fifo_head;
-logic [PRN_BITS-1:0] fifo_tail;
+logic get_prns[MAX_OPERANDS];
 
-// current logical to physical register mapping
-typedef struct packed {
-    logic [PRN_BITS-1:0] prn;
+logic new_prns_valid;
+
+logic [PRN_BITS:0] rem_free_prns;
+fifo #(1<<PRN_BITS, PRN_BITS, MAX_OPERANDS) prn_queue (
+    .clk(clk),
+    .rst(rst),
+    .get_en(get_prns),
+    .put_en(free_valid),
+    .put(free_prns),
+
+    .gotten(prn_output),
+    .gotten_valid(new_prns_valid),
+
+    .len(rem_free_prns)
+);
+
+typedef struct {
     logic valid;
-} Entry;
-Entry lrn_to_prn[1 << 5:0];
+    logic [PRN_BITS-1:0] prn;
+} RemapEntry;
 
-// the number of new registers needed to satisfy lrn_output
-logic [PRN_BITS:0] num_requested_regs;
-// the number of registers being reclaimed
-logic [PRN_BITS:0] num_freed_regs;
-// the number of physical registers remaining IN FIFO
-logic [PRN_BITS:0] phys_remaining;
-// total number of physical registers remaining including reclaimed this cycle
-logic [PRN_BITS:0] phys_available;
-always_comb
-begin
+RemapEntry remap_file[1 << ARN_BITS];
 
-    prn_input = lrn_output;
-    num_requested_regs = (
-        (lrn_output[0] != INVALID_LRN && lrn_output[0] != ZERO_LRN ? 1 : 0)
-        + (lrn_output[1] != INVALID_LRN && lrn_output[1] != ZERO_LRN ? 1 : 0)
-        + (lrn_output[2] != INVALID_LRN && lrn_output[2] != ZERO_LRN ? 1 : 0)
-    );
+logic [MO_BITS-1:0] num_requested_prns;
+logic arn_valid[MAX_OPERANDS];
+always_comb begin
+    // perform the input remapping
+    for (int i = 0; i < MAX_OPERANDS; i++) begin
+        if (arn_input[i] == ARN_BITS'(62)) begin // invalid
+            prn_input_valid[i] = 0;
+            prn_input[i] = (1<<PRN_BITS) - 1;
+        end else if (arn_input[i] == ARN_BITS'(63)) begin // zero
+            prn_input_valid[i] = 0;
+            prn_input[i] = 0;
+        end else begin // valid ARN (0-32) needs remapping
+            prn_input_valid[i] = 1;
+            prn_input[i] = remap_file[arn_input[i]].prn;
+            if (!remap_file[arn_input[i]].valid) begin
+                $display("ERROR: Assigning ARN %0d an empty remap entry!", arn_input[i]);
+            end
+        end
+    end 
 
-    num_freed_regs = 0;
-    for (int i = 0; i < MAX_OPERANDS*2; i++) begin
-        num_freed_regs = free_valid[i] ? num_freed_regs + 1 : num_freed_regs;
+    // arn_valid is per-ARN valid for convenience
+    for (int i = 0; i < MAX_OPERANDS; i++) begin
+        arn_valid[i] = arn_output[i] != ARN_BITS'(62) && arn_output[i] != ARN_BITS'(63);
     end
 
-    if (fifo_tail == fifo_head) begin
-        phys_remaining = 1 << PRN_BITS;
-    end else if (fifo_head < fifo_tail) begin
-        phys_remaining = fifo_tail - fifo_head;
+    // calculate amount of needed PRN's
+    num_requested_prns = 0;
+    for (int i = 0; i < MAX_OPERANDS; i++) begin
+        num_requested_prns = num_requested_prns + arn_valid[i];
+    end
+
+    // mappings cannot be valid if we don't have enough PRN's to spare
+    if ((PRN_BITS+1)'(num_requested_prns) > rem_free_prns) begin
+        mapping_valid = 0;
     end else begin
-        phys_remaining = (1 << PRN_BITS) - (fifo_head - fifo_tail);
+        mapping_valid = 1;
     end
 
-    phys_available = {phys_remaining + num_freed_regs}[PRN_BITS:0];
+    // set getter enable for PRN queue
+    for (int i = 0; i < MAX_OPERANDS; i++) begin
+        get_prns[i] = arn_valid[i] && mapping_valid;
+        // also set prn_output_valid since it's the same
+        prn_output_valid[i] = get_prns[i];
+    end
+
+    // retrieve previous physical register value of remap file to send to ROB
+    for (int i = 0; i < MAX_OPERANDS; i++) begin
+        mapping_inputs_valid[i] = get_prns[i];
+        mapping_inputs_arn[i] = arn_input[i];
+        mapping_inputs_prn[i] = remap_file[arn_input[i]].prn;
+    end
+
+    // do this since we can
+    mapping_valid = new_prns_valid & mapping_valid;
 end
 
 always_ff @(posedge clk)
-begin
-
     if (rst) begin
-        // reset free PRN fifo
-        $display("Resetting PRN FIFO");
-        for (int i = 0; i < (1 << PRN_BITS); i++) begin
-            free_prn_fifo[i] <= 6'(i);
+        $display("Resetting Renamer");
+        for (int i = 0; i < (1 << ARN_BITS); i++) begin
+            remap_file[i].valid <= 0;
         end
-        fifo_head <= 0;
-        fifo_tail <= 0;
-
-        // reset lrn to prn mappings
-        $display("Resetting LRN to PRN mapping");
-        for (int i = 0; i < (1 << 6); i++) begin
-            lrn_to_prn[i].valid <= 0;
-        end
-    end else if (lrns_valid) begin
-        $display("---------------------------------");
-        $display("Number of requested registers: %0d", num_requested_regs);
-        $display("Number of freed registers:     %0d", num_freed_regs);
-        $display("Physical registers in FIFO:    %0d", phys_remaining);
-        $display("Total available registers:     %0d", phys_available);
-        $display("---------------------------------");
-
-        // map lrns to prns for the inputs (simple)
+    end else if (input_valid && mapping_valid) begin // check state change requested and valid
+        // update remap file with newly requested PRN's
         for (int i = 0; i < MAX_OPERANDS; i++) begin
-            prn_input[i] <= lrn_input[i] != INVALID_LRN && lrn_input[i] != ZERO_LRN ? 
-                            lrn_to_prn[lrn_input[i]].prn : 0;
-            if (lrn_input[i] != INVALID_LRN && lrn_input[i] != ZERO_LRN) begin
-                $display("  Logical[%d] mapped to Physical[%d]", lrn_input[i], lrn_to_prn[lrn_input[i]].prn);
+            if (arn_valid[i]) begin
+                remap_file[arn_input[i]].valid <= 1;
+                remap_file[arn_input[i]].prn <= prn_output[i];
             end
-        end
-
-        // handle allocation
-        if (num_requested_regs > phys_available) begin
-            // not enough physical registers remaining to give away
-            prns_valid <= 0;
-        end else begin
-            // allocate the physical registers
-            if (num_requested_regs == num_freed_regs) begin
-                // pass register ownership directly without touching FIFO
-            end else if (num_requested_regs < num_freed_regs) begin
-                // pass needed registers and store rest in FIFO
-            end else if (num_requested_regs > num_freed_regs) begin
-                // take all freed registers and also take from FIFO
-            end
-            prns_valid <= 1;
         end
     end
+begin
+    
 end
 
 endmodule
